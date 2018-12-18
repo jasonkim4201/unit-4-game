6ac0e31d616edd406bb041f6d617dfe80f1734a1
Jason Kim <jasonkim4201@gmail.com>
2018-12-18 01:37:03 -0500

    changes in css files for cleaner html appearance. random number generator added on gem values. added reset function. added groundwork for win lose conditions and notes for on click function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,19 +29,135 @@ $("#winScore").text("Win: " + win);
 $("#loseScore").text("Lose: " + lose);
 $("#playerPoints").text(currentScore);
 
-var gemValue1 = Math.floor(Math.random() * (12 - 1)) + 1;
 // Make an array from 1-12 to ensure each gem will not have same random number?
-var gemValueRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log(gemValueRange);
 /* var ranGemValue = gemValueRange[Math.floor(Math.random() * gemValueRange.length)]; */
+// Math.floor(Math.random() * (max - min + 1)) + min; aka Math.floor(Math.random() * (12)) + 1; ?
+var gemValue1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var gemValue2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
 
-for (var i = 0; i < 4; i++) {
-  var ranGemValue = gemValueRange[Math.floor(Math.random() * gemValueRange.length)];
-  console.log("random gem value:" + ranGemValue); /* THIS METHOD GIVES ME REPEAT VALUES FOR GEMS. Figure it out! */
+if (gemValue1 === gemValue2 ||
+    gemValue1 === gemValue3 ||
+    gemValue1 === gemValue4) {
+      var gemValue2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    }
+if (gemValue2 === gemValue3 ||
+    gemValue2 === gemValue4) {  
+      var gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    }
+if (gemValue3 === gemValue4) {
+  var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+}
+console.log("Gem 1 value: " + gemValue1);
+console.log("Gem 2 value: " + gemValue2);
+console.log("Gem 3 value: " + gemValue3);
+console.log("Gem 4 value: " + gemValue4);
+console.log("Reduced the chance of repeating random values. Yes, I'm pretty sure this is a horrible way to go about it.");
+//Gem pictures
+$("#blueGem").html("<img class='mysteryGem' src='assets/images/sapphire-gem-clipart-1.jpg' alt='sapphire'>");
+$("#redGem").html("<img class='mysteryGem' src='assets/images/redgem.png' alt='ruby'>");
+$("#greenGem").html("<img class='mysteryGem' src='assets/images/green-emerald-stone-transparent-2.png' alt='emerald'>");
+$("#yellowGem").html("<img class='mysteryGem' src='assets/images/yellowgem1.jpg' alt='topaz'>")
+
+// RESET FUNCTION
+function reset() {
+  //current score return to 0
+  currentScore = 0;
+  $("#playerPoints").text(currentScore);
+  //rerolls target number on reset
+  var ranTargetNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  console.log(ranTargetNum);
+  $("#targetNumber").text(ranTargetNum);
+  //rerolls gem values on reset
+  gemValue1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  gemValue2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+  // "attempt" on reducing chance of same value on gems
+  if (gemValue1 === gemValue2 ||
+    gemValue1 === gemValue3 ||
+    gemValue1 === gemValue4) {
+      var gemValue2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    }
+if (gemValue2 === gemValue3 ||
+    gemValue2 === gemValue4) {  
+      var gemValue3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+      var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    }
+if (gemValue3 === gemValue4) {
+  var gemValue4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+}
+  console.log("Gem 1 reset value: " + gemValue1);
+  console.log("Gem 2 reset value: " + gemValue2);
+  console.log("Gem 3 reset value: " + gemValue3);
+  console.log("Gem 4 reset value: " + gemValue4);
 }
 
+//if current score = ranTargetNum then win++ and reset || put in fuction so it doesn't go off immediately
+if (currentScore === ranTargetNum) {
+  win++;
+  reset();
+
+}
+//if current score > ranTargetNum then lose ++ and reset || put in function so it doesn't go off immdiately
+
+if (currentScore > ranTargetNum) {
+  lose++;
+  reset();
+}
+
+//Gem button click aka $(COLORgem).onclick function....
 
 
 
 
 
+
+
+
+
+
+
+
+/* var gemValueRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(gemValueRange);
+for (var i = 0; i < 4; i++) {
+  var ranGemValue = gemValueRange[Math.floor(Math.random() * gemValueRange.length)];
+  console.log("random gem value:" + ranGemValue); 
+
+  var gemPic = $("<img>");
+  gemPic.addClass("mysteryGem");
+  gemPic.attr("src", "assets/images/sapphire-gem-clipart-1.jpg");
+  gemPic.attr("src", "assets/images/redgem.png");
+  gemPic.attr("src", "assets/images/green-emerald-stone-transparent-2.png");
+  gemPic.attr("src", "assets/images/yellowgem1.jpg");
+  gemPic.attr("data-crystalvalue", gemValueRange[i]);
+  $("#gems").append(gemPic);
+}
+
+ */
+
+/* var gemPic = $("<img>");
+  gemPic.addClass("mysteryGem");
+  gemPic.attr("src", "assets/images/sapphire-gem-clipart-1.jpg");
+  gemPic.attr("src", "assets/images/redgem.png");
+  gemPic.attr("src", "assets/images/green-emerald-stone-transparent-2.png");
+  gemPic.attr("src", "assets/images/yellowgem1.jpg");
+  gemPic.attr("data-crystalvalue", gemValueRange[i]);
+  $("#gems").append(gemPic); */
+
+
+/* var gemValue1 = Math.floor(Math.random() * (12 - 1)) + 1;
+var gemValue2 = Math.floor(Math.random() * (12 - 1)) + 1;
+var gemValue3 = Math.floor(Math.random() * (12 - 1)) + 1;
+var gemValue4 = Math.floor(Math.random() * (12 - 1)) + 1;
+console.log(gemValue1);
+console.log(gemValue2);
+console.log(gemValue3);
+console.log(gemValue4); */
